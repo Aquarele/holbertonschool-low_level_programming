22bddc4d152e60f3aa0574266150844453ee745c
Aquarele <laurarubio.c12@gmail.com>
2022-10-25 16:46:52 -0500

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int ent;
+
+	for (ent = '0'; ent <= '9'; ent++)
+	putchar(ent);
+	putchar('\n');
+
+	return (0);
+}
